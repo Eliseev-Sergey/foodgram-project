5ce6227dca818f61b601027596e86aaa82377d63
Eliseev-Sergey <sryes121@gmail.com>
2023-10-29 03:48:17 +0300

    Регистрация по email. Модели CustomUser,Subscriptions и админка

---
+++ backend/users/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """ Пользовательский Manager, который  использует email в качестве
+    уникального идентификатора для аутентификации вместо username.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """ Создать и сохранить пользователя по email и password."""
+        if not email:
+            raise ValueError('Укажите адрес электронной почты!')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """ Создать и сохранить superuser по email и password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    """ Пользовательская модель CustomUser."""
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        max_length=254,
+        unique=True,
+        null=False
+    )
+    username = models.CharField(
+        verbose_name='Уникальный юзернейм',
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+        blank=True
+    )
+    password = models.CharField(
+        verbose_name='Пароль',
+        max_length=150,
+        blank=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+
+class Subscriptions(models.Model):
+    """ Модель подписки на публикации авторов рецептов."""
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='publisher',
+        verbose_name='Автор'
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='subscriber',
+        verbose_name='Подписчик'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.user} подписан на {self.author}'
